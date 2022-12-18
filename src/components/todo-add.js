0e8d11eb188c2ad5072ff32116e2ef0100201305
charlie <ljwcharlie@gmail.com>
2022-12-18 12:01:15 +0800

    fix

--- src/components/todo-add.js
+++ src/components/todo-add.js
@@ -5,15 +5,22 @@ const TodoAdd = ({ list, setList, checked }) => {
   // list 和 setList 是在父组件维护的状态
   const [newListItem, setNewListItem] = useState(""); // 这个state用于获取input的值，即设置受控组件
 
-  // 获取用户input输入
+  // onchange事件的回调，用于获取用户input输入
   const handleNewListItem = (e) => {
     setNewListItem(e.target.value);
   };
 
-  // 将用户输入显示出来
-  const handleItemAdd = () => {
+  // 按下回车键新增内容
+  const handleKeyDown = (e) => {
+    if (e.keyCode !== 13) return; // 如果用户按的不是enter，则程序结束
+    if (e.target.value.trim() === "") {
+      // 如果用户没有输入内容，则程序结束
+      alert("您还没有输入内容"); // 提醒用户输入内容
+      return;
+    }
     setList([
-      ...list, // 这个写法的意思是把list复制出来一份，再拼接上新的内容
+      // 主要逻辑
+      ...list,
       {
         id: nanoid(),
         item: newListItem,
@@ -22,6 +29,23 @@ const TodoAdd = ({ list, setList, checked }) => {
     ]);
     setNewListItem("");
   };
+
+  // 点击添加按钮新增内容
+  const handleItemAdd = () => {
+    if (newListItem === "") {
+      alert("您还没有输入内容");
+      return;
+    }
+    setList([
+      ...list, // 将用户输入拼接到list数组里面。这个写法的意思是把list复制出来一份，再拼接上新的内容
+      {
+        id: nanoid(),
+        item: newListItem,
+        Checked: checked,
+      },
+    ]);
+    setNewListItem(""); // 新增完成后将input清空
+  };
   return (
     <>
       <input
@@ -29,6 +53,7 @@ const TodoAdd = ({ list, setList, checked }) => {
         placeholder="添加一条todo"
         value={newListItem}
         onChange={handleNewListItem}
+        onKeyDown={handleKeyDown}
       />
       <button onClick={handleItemAdd}>添加</button>
     </>
--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -1,19 +1,23 @@
 import { useState } from "react";
 
+// 接收props的简写方式
 const TodoItem = ({ list, setList, item, index, setChecked }) => {
-  const [isCheck, setIsCheck] = useState(false);
+  const [isCheck, setIsCheck] = useState(false); // 用于获取checked的中间状态
+  // 删除按钮的点击事件
   const handleDelete = () => {
     const newList = [...list]; // 注意不能直接修改props，要浅拷贝一份出来
     newList.splice(index, 1); // 操作新的数组
     setList(newList); // 将新的状态（数组）传给父组件（setList是维护父组件state的方法）
   };
+  // input控件必需的onchange事件
   const handleChange = (e) => {
-    setIsCheck(e.target.checked);
-    setChecked(isCheck);
+    setIsCheck(e.target.checked); // 更新isCheck的值，实现表单数据的双向绑定
+    setChecked(isCheck); // 将isCheck的状态同步给父组件
   };
 
   return (
     <li style={{ listStyle: "none" }}>
+      {/* 将isCheck和checked绑定 */}
       <input type="checkbox" checked={isCheck} onChange={handleChange} />
       <span style={{ textDecorationLine: isCheck ? "line-through" : "none" }}>
         {item.item}
